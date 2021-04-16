01d43f7d64002bf0cf111387c886cf18d21c2420
Running water <alsgks1218@naver.com>
2021-04-16 15:12:14 +0900

    add scroll event

---
+++ js/wall3d.js
@@ -0,0 +1,16 @@
+(function () {
+  const houseElem = document.querySelector(".house");
+  let maxScrollValue = 0;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
+
+  window.addEventListener("scroll", function () {
+    const zMove = (pageYOffset / maxScrollValue) * 980 - 490;
+    houseElem.style.transform = `translateZ(${zMove}vw)`;
+  });
+
+  window.addEventListener("resize", resizeHandler);
+  resizeHandler();
+})();
